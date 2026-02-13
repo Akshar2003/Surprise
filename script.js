69c67d4fe1eede4e5614eecbc9a80672464b7977
Akshar2003 <109605249+Akshar2003@users.noreply.github.com>
2026-02-13 22:19:56 +0530

    Update messages with new prompts and emojis

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
+    "Vaishu plsss🥺",
     "Pookie please...",
     "Just think about it!",
-    "If you say no, I will be really sad...",
+    "If you say no, I will be really sad🥺...",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
